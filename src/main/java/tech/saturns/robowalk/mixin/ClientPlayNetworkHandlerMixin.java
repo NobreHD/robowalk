a56cc8b47792236125693688abec9e35d40d1a40
Leonardo Nobre <60514067+NobreHD@users.noreply.github.com>
2022-09-15 19:46:56 +0100

    Remove message commands

--- src/main/java/tech/saturns/robowalk/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/tech/saturns/robowalk/mixin/ClientPlayNetworkHandlerMixin.java
@@ -21,56 +21,58 @@ public class ClientPlayNetworkHandlerMixin {
     MinecraftClient instance = MinecraftClient.getInstance();
 
     @Inject(at = @At("HEAD"), method = "sendPacket", cancellable = true)
-    private void onSendPacket(Packet<?> rawpacket, CallbackInfo callback){
-        if(rawpacket instanceof PlayerMoveC2SPacket packet){
-            if(packet instanceof PlayerMoveC2SPacket.PositionAndOnGround || packet instanceof PlayerMoveC2SPacket.Full){
-                if(Main.enabled){
+    private void onSendPacket(Packet<?> rawpacket, CallbackInfo callback) {
+        if (rawpacket instanceof PlayerMoveC2SPacket packet) {
+            if (packet instanceof PlayerMoveC2SPacket.PositionAndOnGround || packet instanceof PlayerMoveC2SPacket.Full) {
+                if (Main.enabled) {
                     callback.cancel();
                     double x = Math.round(packet.getX(0) * 100.0) / 100.0; //round packets as best we can
                     double z = Math.round(packet.getZ(0) * 100.0) / 100.0;
 
 
-                    if(Main.debug) sendChatMessage2("Sent [X/Z]: " + x + ":" + z); //Main.debug log
+                    if (Main.debug) Main.sendMsg("Sent [X/Z]: " + x + ":" + z); //Main.debug log
 
-                    long dx = ((long)(x * 1000)) % 10; //simulate the check that liveoverflow runs 
-                    long dz = ((long)(z * 1000)) % 10;
-                    if(Main.debug) sendChatMessage2("Calculated [X/Z]: " + dx + ":" + dz);
+                    long dx = ((long) (x * 1000)) % 10; //simulate the check that liveoverflow runs
+                    long dz = ((long) (z * 1000)) % 10;
+                    if (Main.debug) Main.sendMsg("Calculated [X/Z]: " + dx + ":" + dz);
 
 
-                    if(dx != 0 || dz != 0){
-                        if(Main.debug) sendChatMessage2("Found packet [DX/DZ] != 0, Modification Failed!, Aborting!!"); //drop these weird packets that sometimes get through 
+                    if (dx != 0 || dz != 0) {
+                        if (Main.debug)
+                            Main.sendMsg("§4Found packet [DX/DZ] != 0, Modification Failed!, Aborting!!"); //drop these weird packets that sometimes get through
                         return;
                     }
 
                     Packet<?> clone;
 
-                    if(packet instanceof PlayerMoveC2SPacket.PositionAndOnGround){
+                    if (packet instanceof PlayerMoveC2SPacket.PositionAndOnGround) {
                         clone = new PlayerMoveC2SPacket.PositionAndOnGround(x, packet.getY(0), z, packet.isOnGround());
-                    }else{ 
+                    } else {
                         clone = new PlayerMoveC2SPacket.Full(x, packet.getY(0), z, packet.getYaw(0), packet.getPitch(0), packet.isOnGround());
                     }
 
 
                     instance.player.networkHandler.getConnection().send(clone);
                 }
-            }   
+            }
         }
-        if(rawpacket instanceof VehicleMoveC2SPacket packet){
-            if(Main.enabled){
+        if (rawpacket instanceof VehicleMoveC2SPacket packet) {
+            if (Main.enabled) {
                 callback.cancel();
                 double x = Math.round(packet.getX() * 100.0) / 100.0; //round packets as best we can
                 double z = Math.round(packet.getZ() * 100.0) / 100.0;
 
 
-                if(Main.debug) sendChatMessage2("Sent [Vehicle] [X/Z]: " + x + ":" + z); //Main.debug log
+                if (Main.debug) Main.sendMsg("Sent [Vehicle] [X/Z]: " + x + ":" + z); //Main.debug log
 
-                long dx = ((long)(x * 1000)) % 10; //simulate the check that liveoverflow runs 
-                long dz = ((long)(z * 1000)) % 10;
-                if(Main.debug) sendChatMessage2("Calculated [Vehicle] [X/Z]: " + dx + ":" + dz);
+                long dx = ((long) (x * 1000)) % 10; //simulate the check that liveoverflow runs
+                long dz = ((long) (z * 1000)) % 10;
+                if (Main.debug) Main.sendMsg("Calculated [Vehicle] [X/Z]: " + dx + ":" + dz);
 
 
-                if(dx != 0 || dz != 0){
-                    if(Main.debug) sendChatMessage2("Found packet [Vehicle] [DX/DZ] != 0, Modification Failed!, Aborting!!"); //drop these weird packets that sometimes get through 
+                if (dx != 0 || dz != 0) {
+                    if (Main.debug)
+                        Main.sendMsg("§4Found packet [Vehicle] [DX/DZ] != 0, Modification Failed!, Aborting!!"); //drop these weird packets that sometimes get through
                     return;
                 }
 
@@ -83,42 +85,5 @@ public class ClientPlayNetworkHandlerMixin {
                 instance.player.networkHandler.getConnection().send(movepacket);
             }
         }
-        if(rawpacket instanceof ChatMessageC2SPacket packet){
-            String chat = packet.getChatMessage();
-            System.out.println(chat);
-            if(chat.equalsIgnoreCase("robowalk")){
-                sendChatMessage("robowalk is installed and running!");
-                callback.cancel();
-            }
-            if(chat.equalsIgnoreCase("robowalk on")){
-                Main.enabled = true;
-                sendChatMessage("Movement is now robotic!");
-                callback.cancel();
-            } 
-            if(chat.equalsIgnoreCase("robowalk debug on")){
-                sendChatMessage("Enabling Main.debug mode!");
-                Main.debug = true;
-                callback.cancel();
-            }
-            if(chat.equalsIgnoreCase("robowalk debug off")){
-                Main.debug = false;
-                sendChatMessage("Disabling Main.debug mode!");
-                callback.cancel();
-            }
-            if(chat.equalsIgnoreCase("robowalk off")){
-                Main.enabled = false;
-                sendChatMessage("Robotic movement is now disabled, you can play normally!");
-                callback.cancel();
-            } 
-        }
-    }
-
-
-    void sendChatMessage(String message){
-        instance.player.sendMessage(Text.of(message), true);
-    }
-
-    void sendChatMessage2(String message){
-        instance.player.sendMessage(Text.of(message), false);
     }
 }
